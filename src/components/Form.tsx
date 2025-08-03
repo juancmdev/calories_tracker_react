861ed060e4e946763b15fd4d428156072e21cdea
juancmdev <juancmorenoj@gmail.com>
2025-08-03 12:18:21 -0500

    updated

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,15 +4,26 @@ export default function Form() {
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="category">Categoría:</label>
+        <label htmlFor="category" className="font-bold">
+          Categoría:
+        </label>
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
         >
           {categories.map((category) => (
-            <option>{category.name}</option>
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
           ))}
         </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Actividad:
+        </label>
+        <input />
       </div>
     </form>
   );
